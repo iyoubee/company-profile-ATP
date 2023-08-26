67c7c40f245f1194fab1b086df2f449e5db4207d
iyoubee <al.ayubi2020@gmail.com>
2023-08-26 08:02:32 +0700

    done all (belum mobile)

--- src/components/elements/PartnerIndex/index.tsx
+++ src/components/elements/PartnerIndex/index.tsx
@@ -30,27 +30,29 @@ export const PartnerIndex: React.FC = () => {
   return (
     <>
       <div className="w-full mt-[128px]">
-        <Slider {...settings} ref={sliderRef} className="tes">
-          <div className="w-[480px] h-[360px]">
-            <PartnerCard />
-          </div>
-          <div className="w-[480px] h-[360px]">
-            <PartnerCard />
-          </div>
-          <div className="w-[480px] h-[360px]">
-            <PartnerCard />
-          </div>
-          <div className="w-[480px] h-[360px]">
-            <PartnerCard />
-          </div>
-          <div className="w-[480px] h-[360px]">
-            <PartnerCard />
-          </div>
-          <div className="w-[480px] h-[360px]">
-            <PartnerCard />
-          </div>
-        </Slider>
-        <div className="w-full px-20 py-[32px] bg-[#73A1C3] flex gap-[64px] relative">
+        <Reveal>
+          <Slider {...settings} ref={sliderRef} className="tes">
+            <div className="w-[480px] h-[360px]">
+              <PartnerCard />
+            </div>
+            <div className="w-[480px] h-[360px]">
+              <PartnerCard />
+            </div>
+            <div className="w-[480px] h-[360px]">
+              <PartnerCard />
+            </div>
+            <div className="w-[480px] h-[360px]">
+              <PartnerCard />
+            </div>
+            <div className="w-[480px] h-[360px]">
+              <PartnerCard />
+            </div>
+            <div className="w-[480px] h-[360px]">
+              <PartnerCard />
+            </div>
+          </Slider>
+        </Reveal>
+        <div className="w-full px-20 py-[32px] bg-[#73A1C3] relative">
           <button
             className="bg-[#294696] p-[6px] rounded-full absolute -top-5 left-20"
             onClick={prev}
@@ -67,17 +69,22 @@ export const PartnerIndex: React.FC = () => {
               <ChevronWhiteBig />
             </Reveal>
           </button>
-          <p className="text-[#294696] text-[64px] font-bold leading-[86.4px] flex-none">
-            Partner Kami
-          </p>
-          <p className="text-[#FFFAEB] font-medium text-[18px] text-justify leading-[24.3px]">
-            Kami sangat berterima kasih atas testimoni positif dari pelanggan
-            kami. Kami selalu bekerja keras untuk meningkatkan produk dan
-            layanan kami, dan menjadi kepuasan bagi kami ketika mendengar bahwa
-            pelanggan kami puas. Kami berkomitmen untuk memberikan pelanggan
-            kami pengalaman produk terbaik yang kami bisa berikan, dan kami
-            berharap Anda akan terus mengunjungi kami dan menikmati produk kami.
-          </p>
+          <Reveal>
+            <div className="flex gap-[64px]">
+              <p className="text-[#294696] text-[64px] font-bold leading-[86.4px] flex-none">
+                Partner Kami
+              </p>
+              <p className="text-[#FFFAEB] font-medium text-[18px] text-justify leading-[24.3px]">
+                Kami sangat berterima kasih atas testimoni positif dari
+                pelanggan kami. Kami selalu bekerja keras untuk meningkatkan
+                produk dan layanan kami, dan menjadi kepuasan bagi kami ketika
+                mendengar bahwa pelanggan kami puas. Kami berkomitmen untuk
+                memberikan pelanggan kami pengalaman produk terbaik yang kami
+                bisa berikan, dan kami berharap Anda akan terus mengunjungi kami
+                dan menikmati produk kami.
+              </p>
+            </div>
+          </Reveal>
         </div>
       </div>
     </>
