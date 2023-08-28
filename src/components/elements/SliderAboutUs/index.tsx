73c68ea1b59d2f5ab63be3b657b0dcb2cd00b879
yudiptr <94793172+yudiptr@users.noreply.github.com>
2023-08-29 00:06:40 +0700

    pretiter

--- src/components/elements/SliderAboutUs/index.tsx
+++ src/components/elements/SliderAboutUs/index.tsx
@@ -1,8 +1,5 @@
 'use client'
 import React from 'react'
-import { useTranslations } from 'next-intl'
-import { Reveal } from '../Reveal'
-import Image from 'next/image'
 
 export const SliderAboutUs: React.FC = () => {
   return (
@@ -15,59 +12,59 @@ export const SliderAboutUs: React.FC = () => {
           ></div>
           <div className="w-[120px] lg:w-[230px] min-h-[110px] lg:min-h-[200px] relative float-left">
             <div className="w-[20px] h-[20px] lg:w-[48px] lg:h-[48px] absolute bottom-0 left-[60px] lg:left-[115px] translate-x-[-50%] lg:translate-y-[25%] bg-[#E9E0D7] rounded-full shadow-lg"></div>
-                <div
-                    id="frame-event"
-                    className="absolute w-full h-[80px] lg:h-[120px] p-[15px] shadow-xl text-black  flex flex-col gap-2 justify-center items-center bg-[#E9E0D7] rounded-[16px]"
-                >
-                    <span className="text-[10px] lg:text-xl font-extrabold text-center">
-                        titel
-                    </span>
-                    <span className="font-semibold text-[8px] lg:text-lg text-black">
-                        date
-                    </span>
-                </div>
+            <div
+              id="frame-event"
+              className="absolute w-full h-[80px] lg:h-[120px] p-[15px] shadow-xl text-black  flex flex-col gap-2 justify-center items-center bg-[#E9E0D7] rounded-[16px]"
+            >
+              <span className="text-[10px] lg:text-xl font-extrabold text-center">
+                titel
+              </span>
+              <span className="font-semibold text-[8px] lg:text-lg text-black">
+                date
+              </span>
+            </div>
           </div>
           <div className="w-[120px] lg:w-[230px] min-h-[110px] lg:min-h-[200px] relative float-left">
             <div className="w-[20px] h-[20px] lg:w-[48px] lg:h-[48px] absolute bottom-0 left-[60px] lg:left-[115px] translate-x-[-50%] lg:translate-y-[25%] bg-[#E9E0D7] rounded-full shadow-lg"></div>
-                <div
-                    id="frame-event"
-                    className="absolute w-full h-[80px] lg:h-[120px] p-[15px] shadow-xl text-black  flex flex-col gap-2 justify-center items-center bg-[#E9E0D7] rounded-[16px]"
-                >
-                    <span className="text-[10px] lg:text-xl font-extrabold text-center">
-                        titel
-                    </span>
-                    <span className="font-semibold text-[8px] lg:text-lg text-black">
-                        date
-                    </span>
-                </div>
+            <div
+              id="frame-event"
+              className="absolute w-full h-[80px] lg:h-[120px] p-[15px] shadow-xl text-black  flex flex-col gap-2 justify-center items-center bg-[#E9E0D7] rounded-[16px]"
+            >
+              <span className="text-[10px] lg:text-xl font-extrabold text-center">
+                titel
+              </span>
+              <span className="font-semibold text-[8px] lg:text-lg text-black">
+                date
+              </span>
+            </div>
           </div>
           <div className="w-[120px] lg:w-[230px] min-h-[110px] lg:min-h-[200px] relative float-left">
             <div className="w-[20px] h-[20px] lg:w-[48px] lg:h-[48px] absolute bottom-0 left-[60px] lg:left-[115px] translate-x-[-50%] lg:translate-y-[25%] bg-[#E9E0D7] rounded-full shadow-lg"></div>
-                <div
-                    id="frame-event"
-                    className="absolute w-full h-[80px] lg:h-[120px] p-[15px] shadow-xl text-black  flex flex-col gap-2 justify-center items-center bg-[#E9E0D7] rounded-[16px]"
-                >
-                    <span className="text-[10px] lg:text-xl font-extrabold text-center">
-                        titel
-                    </span>
-                    <span className="font-semibold text-[8px] lg:text-lg text-black">
-                        date
-                    </span>
-                </div>
+            <div
+              id="frame-event"
+              className="absolute w-full h-[80px] lg:h-[120px] p-[15px] shadow-xl text-black  flex flex-col gap-2 justify-center items-center bg-[#E9E0D7] rounded-[16px]"
+            >
+              <span className="text-[10px] lg:text-xl font-extrabold text-center">
+                titel
+              </span>
+              <span className="font-semibold text-[8px] lg:text-lg text-black">
+                date
+              </span>
+            </div>
           </div>
           <div className="w-[120px] lg:w-[230px] min-h-[110px] lg:min-h-[200px] relative float-left">
             <div className="w-[20px] h-[20px] lg:w-[48px] lg:h-[48px] absolute bottom-0 left-[60px] lg:left-[115px] translate-x-[-50%] lg:translate-y-[25%] bg-[#E9E0D7] rounded-full shadow-lg"></div>
-                <div
-                    id="frame-event"
-                    className="absolute w-full h-[80px] lg:h-[120px] p-[15px] shadow-xl text-black  flex flex-col gap-2 justify-center items-center bg-[#E9E0D7] rounded-[16px]"
-                >
-                    <span className="text-[10px] lg:text-xl font-extrabold text-center">
-                        titel
-                    </span>
-                    <span className="font-semibold text-[8px] lg:text-lg text-black">
-                        date
-                    </span>
-                </div>
+            <div
+              id="frame-event"
+              className="absolute w-full h-[80px] lg:h-[120px] p-[15px] shadow-xl text-black  flex flex-col gap-2 justify-center items-center bg-[#E9E0D7] rounded-[16px]"
+            >
+              <span className="text-[10px] lg:text-xl font-extrabold text-center">
+                titel
+              </span>
+              <span className="font-semibold text-[8px] lg:text-lg text-black">
+                date
+              </span>
+            </div>
           </div>
         </div>
       </div>
