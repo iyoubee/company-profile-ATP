10232561fd977a628762bc27b26f9456fcd8abb9
yudiptr <94793172+yudiptr@users.noreply.github.com>
2023-08-31 10:18:07 +0700

    done about us product minus marquee

--- src/components/elements/AboutUsProduct/index.tsx
+++ src/components/elements/AboutUsProduct/index.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import React from 'react'
 import { Reveal } from '../Reveal'
 import { useTranslations } from 'next-intl'
+import Slider from 'react-slick'
 
 export const AboutUsProduct: React.FC = () => {
   const t = useTranslations('About.product')
@@ -10,7 +11,7 @@ export const AboutUsProduct: React.FC = () => {
   return (
     <>
       <section className="h-fit bg-[#294696] bg-cover relative p-12">
-        <div className="w-full flex" id="history">
+        <div className="w-full flex justify-between" id="history">
           <div className="w-1/3">
             <Reveal>
               <p className=" text-[#D6E8F2] text-[48px] leading-[64.8px] font-bold mb-5">
@@ -23,10 +24,55 @@ export const AboutUsProduct: React.FC = () => {
               </p>
             </Reveal>
           </div>
-
-          <div className="flex justify-center w-2/3"></div>
+          <div className=" w-5/12 z-20">
+            <Slider
+            infinite
+            speed={1000}
+            arrows={true}
+            slidesToShow={1}
+            slidesToScroll={1}
+            autoplay
+            autoplaySpeed={10000}
+            dots
+            >
+              <div className='w-fit aspect-[2/1] relative bg-[#D6E8F2] p-6 rounded-2xl'>
+                <div className='relative h-[400px] w-full'>
+                  <Image
+                        src={'/placeholder.jpeg'}
+                        fill
+                        alt="placeholder"
+                        className="object-cover relative"
+                      />
+                </div>
+                <p className='text-[#294696]'>{t('1')}</p>
+              </div>
+              <div className='w-fit aspect-[2/1] relative bg-[#D6E8F2] p-6 rounded-2xl'>
+                <div className='relative h-[400px] w-full'>
+                  <Image
+                        src={'/placeholder.jpeg'}
+                        fill
+                        alt="placeholder"
+                        className="object-cover relative"
+                      />
+                </div>
+                <p className='text-[#294696]'>{t('2')}</p>
+              </div>
+              <div className='w-fit aspect-[2/1] relative bg-[#D6E8F2] p-6 rounded-2xl'>
+                <div className='relative h-[400px] w-full'>
+                  <Image
+                        src={'/placeholder.jpeg'}
+                        fill
+                        alt="placeholder"
+                        className="object-cover relative"
+                      />
+                </div>
+                <p className='text-[#294696]'>{t('3')}</p>
+              </div>
+             
+            </Slider>
+          </div>
           <div className="">
-            <Image src={'/Vector.svg'} fill className="" alt="Assets" />
+            <Image src={'/Vector.svg'} fill className=" translate-y-[175px]" alt="Assets" />
           </div>
         </div>
       </section>
