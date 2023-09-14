7e88310d44216f72da6d53a89cbf7cc6088f6b79
yudiptr <94793172+yudiptr@users.noreply.github.com>
2023-09-14 22:13:30 +0700

    fix:footer and about us

--- src/components/elements/Footer/index.tsx
+++ src/components/elements/Footer/index.tsx
@@ -1,16 +1,19 @@
 'use client'
 import Image from 'next/image'
+import { useTranslations } from 'next-intl'
 import React from 'react'
 
 export const Footer: React.FC = () => {
+
+  const t = useTranslations('Footer')
   return (
     <>
       <footer className="footer bg-[#294696] relative border-b-2 border-blue-700 lg:items-center flex flex-col">
         <div className="container px-3">
           <div className="sm:flex sm:mt-8">
-            <div className="mt-8 sm:mt-0 sm:w-full sm:px-8 flex flex-col md:flex-row justify-between gap-4">
+            <div className="mt-8 sm:mt-0 sm:w-full sm:px-8 flex flex-col lg:flex-row justify-between gap-4">
               <div className="flex flex-col w-full">
-                <div className="text-[#D6E8F2] font-bold text-4xl mb-2 flex gap-2">
+                <div className="text-[#D6E8F2] items-center font-bold text-2xl md:text-3xl mb-2 flex gap-2">
                   <div className="">
                     <Image
                       className=""
@@ -30,8 +33,8 @@ export const Footer: React.FC = () => {
                 </span>
               </div>
               <div className="flex flex-col w-full lg:w-2/4">
-                <span className="text-[#D6E8F2] font-bold text-4xl mb-2">
-                  Temui Kami
+                <span className="text-[#D6E8F2] font-bold text-2xl md:text-3xl mb-2">
+                  {t('temui')}
                 </span>
                 <span className=" font-medium">
                   <p className="text-[#D6E8F2]  text-md  ">
@@ -40,41 +43,49 @@ export const Footer: React.FC = () => {
                   </p>
                 </span>
               </div>
-              <div className="flex lg:justify-center md:justify-center sm:justify-normal w-full lg:w-3/5">
-                <div className="flex flex-col">
-                  <span className="text-[#D6E8F2] font-bold text-4xl mb-2">
-                    Kontak Kami
+              <div className="flex flex-col  w-full lg:w-1/3 lg:translate-x-8">
+                <span className="text-[#D6E8F2] font-bold text-2xl md:text-3xl mb-2">
+                {t('informasi.title')}
+                </span>
+                <span className=" font-medium">
+                  <a
+                    href="#"
+                    className="text-[#D6E8F2]  text-md hover:text-blue-500"
+                  >
+                    {t('informasi.tentang')}
+                  </a>
+                </span>
+                <span className=" font-medium">
+                  <a
+                    href="#"
+                    className="text-[#D6E8F2]  text-md hover:text-blue-500"
+                  >
+                    {t('informasi.produk')}
+                  </a>
+                </span>
+                <span className=" font-medium">
+                  <a
+                    href="#"
+                    className="text-[#D6E8F2]  text-md hover:text-blue-500"
+                  >
+                    {t('informasi.kontak')}
+                  </a>
+                </span>
+              </div>
+              <div className="flex justify-normal w-full lg:w-3/5 ">
+                <div className="flex flex-col lg:translate-x-8">
+                  <span className="text-[#D6E8F2] font-bold text-2xl md:text-3xl mb-2">
+                  {t('kontak')}
                   </span>
                   <span className=" font-medium">
-                    <p className="text-[#D6E8F2] text-md">+6281323583457</p>
+                    <p className="text-[#D6E8F2] text-md">pt.atpidn@gmail.com</p>
                   </span>
                 </div>
-              </div>
-              <div className="flex flex-col  w-full lg:w-2/4">
-                <span className="text-[#D6E8F2] font-bold text-4xl mb-2">
-                  Ikuti Kami
-                </span>
-                <span className=" font-medium">
-                  <a
-                    href="#"
-                    className="text-[#D6E8F2]  text-md hover:text-blue-500"
-                  >
-                    Instagram
-                  </a>
-                </span>
-                <span className=" font-medium">
-                  <a
-                    href="#"
-                    className="text-[#D6E8F2]  text-md hover:text-blue-500"
-                  >
-                    Facebook
-                  </a>
-                </span>
               </div>
             </div>
           </div>
         </div>
-        <div className="container mx-auto lg:px-10 ">
+        <div className="container md:px-9 sm:px-9 px-2 lg:px-10 ">
           <div className="mt-4 flex flex-col">
             <div className="py-6">
               <p className="text-sm text-white font-bold mb-2">
--- src/components/elements/LocationIndex/index.tsx
+++ src/components/elements/LocationIndex/index.tsx
@@ -8,7 +8,7 @@ export const LocationIndex: React.FC = () => {
   return (
     <>
       <section className="flex justify-center">
-        <div className="flex bg-[#294696] rounded-2xl flex-col-reverse lg:flex-row">
+        <div className="flex bg-[#294696] rounded-2xl flex-col-reverse lg:flex-row pt-4 lg:pt-0">
           <div className="md:w-[500px] w-[300px] bg-[#63A3C7] rounded-2xl md:p-12 p-3 flex flex-col gap-4">
             <div className="flex flex-col gap-1">
               <p className="text-xl font-bold text-white">
@@ -37,30 +37,31 @@ export const LocationIndex: React.FC = () => {
                 <IconPhoneCall color="white" /> +62 812 9828 2870
               </p>
               <p className="flex gap-4 items-center text-white">
-                <IconMail color="white" /> yudiputrasabri789@gmail.com
+                <IconMail color="white" /> ptaceng@gmail.com
               </p>
               <div className="flex gap-4 items-center text-white">
                 <IconLocation color="white" />{' '}
                 <div>
-                  Jalan Cipinang Muara 2 No.3A,<p>Jakarta Timur</p>
+                132 Dartmouth Street Boston,<p>Massachusetts 02156 United States</p>
                 </div>
               </div>
             </div>
             <div className="w-full text-white">
               <p className="font-bold text-xl">Our Email</p>
-              <div className=" border-b-2 md:p-4 pl-2 items-center gap-4 w-full">
-                yudiputrasabri789@gmail.com
-              </div>
-              <div className="w-full flex justify-end hover:cursor-pointer mt-3">
+              <div className=" border-b-2 md:p-4 px-2 py-1 gap-4 w-full flex items-center">
+                <span className='text-xs md:text-sm lg:text-lg'>ptaceng@gmail.com</span>
+                <div className="w-full hover:cursor-pointer flex justify-end">
                 <div
-                  className=" w-fit bg-[#63A3C7] p-2 rounded-lg"
+                  className=" w-fit bg-[#63A3C7] md:p-2 p-1 rounded-lg flex items-center justify-center "
                   onClick={() => {
                     navigator.clipboard.writeText('yudiputrasabri789@gmail.com')
                   }}
                 >
-                  <p>Copy Email</p>
+                  <p className='text-xs md:text-sm lg:text-lg text-center'>Copy</p>
                 </div>
               </div>
+              </div>
+              
             </div>
           </div>
         </div>
